5cd9b59df905b1bb66fa2853d8ef2491446b2878
kangwon.seo <kangwon.seo@lge.com>
2020-08-18 12:19:28 +0900

    memento

---
+++ Memento/memento.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+using namespace std;
+
+class Memento
+{
+  int balance;
+public:
+  Memento(int balance)
+    : balance(balance)
+  {
+  }
+  friend class BankAccount;
+  friend class BankAccount2;
+};
+
+class BankAccount
+{
+  int balance = 0;
+public:
+  explicit BankAccount(const int balance)
+    : balance(balance)
+  {
+  }
+
+  Memento deposit(int amount)
+  {
+    balance += amount;
+    return { balance };
+  }
+
+  void restore(const Memento& m)
+  {
+    balance = m.balance;
+  }
+
+  friend ostream& operator<<(ostream& os, const BankAccount& obj)
+  {
+    return os << "balance: " << obj.balance;
+  }
+};
+
+// undo/redo ===================================
+
+class BankAccount2 
+{
+  int balance = 0;
+  vector<shared_ptr<Memento>> changes;
+  int current;
+public:
+  explicit BankAccount2(const int balance)
+  : balance(balance)
+  {
+    changes.emplace_back(make_shared<Memento>(balance));
+    current = 0;
+  }
+
+  shared_ptr<Memento> deposit(int amount)
+  {
+    balance += amount;
+    auto m = make_shared<Memento>(balance);
+    changes.push_back(m);
+    ++current;
+    return m;
+  }
+
+  void restore(const shared_ptr<Memento>& m)
+  {
+    if (m)
+    {
+      balance = m->balance;
+      changes.push_back(m);
+      current = changes.size() - 1;
+    }
+  }
+
+  shared_ptr<Memento> undo()
+  {
+    if (current > 0)
+    {
+      --current;
+      auto m = changes[current];
+      balance = m->balance;
+      return m;
+    }
+    return{};
+  }
+
+  shared_ptr<Memento> redo()
+  {
+    if (current + 1 < changes.size())
+    {
+      ++current;
+      auto m = changes[current];
+      balance = m->balance;
+      return m;
+    }
+    return{};
+  }
+
+  friend ostream& operator<<(ostream& os, const BankAccount2& obj)
+  {
+    return os << "balance: " << obj.balance;
+  }
+};
+
+void undo_redo()
+{
+  BankAccount2 ba{ 100 };
+  ba.deposit(50);
+  ba.deposit(25); // 125
+  cout << ba << "\n";
+
+  ba.undo();
+  cout << "Undo 1: " << ba << "\n";
+  ba.undo();
+  cout << "Undo 2: " << ba << "\n";
+  ba.redo();
+  cout << "Redo 2: " << ba << "\n";
+
+  ba.undo();
+}
+
+int main()
+{
+  undo_redo();
+
+  getchar();
+  return 0;
+}
